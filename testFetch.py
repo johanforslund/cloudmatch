174b7c72289707c202ac15b9bb82fa212825ca21
johanforslund <johanforslund95@gmail.com>
2019-12-11 15:14:08 +0100

    Add more data from query

--- testFetch.py
+++ testFetch.py
@@ -4,7 +4,7 @@ import json
 
 client_id = 'csSEELZJ9CZQAJnUIssLDhWdk2oSkPC9'
 currentGenre = 'rock'
-params = { 'limit' : '50', 'genres': currentGenre }
+params = { 'limit' : '20', 'genres': currentGenre }
 tracks = requests.get('https://api.soundcloud.com/tracks?client_id=' + client_id, params).json()
 
 result = []
@@ -14,7 +14,7 @@ for count, track in enumerate(tracks):
 
     favoriters = requests.get('https://api.soundcloud.com/tracks/' + str(track['id']) + '/favoriters?client_id=' + client_id, params).json()
 
-    for i in range(0, 10):
+    for i in range(0, 500):
         if 'next_href' not in favoriters.keys() or 'collection' not in favoriters.keys():
             break
 
@@ -22,16 +22,21 @@ for count, track in enumerate(tracks):
         favoriters = requests.get(next_href).json()
 
         favoritersIds = ([favoriter['id'] for favoriter in favoriters['collection']])
+        
+        if i%10 == 0:
+            print('Inner loop: ' + str(i))
 
 
     trackInfo = {}
     trackInfo['id'] = track['id']
     trackInfo['genre'] = track['genre']
+    trackInfo['playback_count'] = track['playback_count']
+    trackInfo['favoritings_count'] = track['favoritings_count']
     trackInfo['favoriters'] = favoritersIds
     result.append(trackInfo)
-    print(len(result))
+    print('Outer loop: ' + str(count))
     time.sleep(0.05)
 
 
-with open('output.json', 'w', encoding='utf8') as json_file:
+with open(currentGenre + '.json', 'w', encoding='utf8') as json_file:
     json.dump(result, json_file, ensure_ascii=False)
