63ea5281ae072f98946ffbb8caf77e2d4d88e869
johanforslund <johanforslund95@gmail.com>
2019-12-17 12:39:25 +0100

    Change algorithm to fetch from users instead

--- testFetch.py
+++ testFetch.py
@@ -2,41 +2,51 @@ import requests
 import time
 import json
 
-client_id = 'csSEELZJ9CZQAJnUIssLDhWdk2oSkPC9'
+client_id = 'cEIhboHJ92pcRNyYQhmg5gYgL69kQlrM'
 currentGenre = 'pop'
-params = { 'limit' : '20', 'genres': currentGenre }
+params = { 'limit' : '200', 'genres': currentGenre }
 tracks = requests.get('https://api.soundcloud.com/tracks?client_id=' + client_id, params).json()
 
 result = []
 
-for count, track in enumerate(tracks):
-    params = {'limit': '200', 'linked_partitioning': '1'}
+track = tracks[-1]
+params = {'limit': '200', 'linked_partitioning': '1'}
 
-    favoriters = requests.get('https://api.soundcloud.com/tracks/' + str(track['id']) + '/favoriters?client_id=' + client_id, params).json()
+favoriters = requests.get('https://api.soundcloud.com/tracks/' + str(track['id']) + '/favoriters?client_id=' + client_id, params).json()
 
-    for i in range(0, 500):
-        if 'next_href' not in favoriters.keys() or 'collection' not in favoriters.keys():
-            break
+params = {'limit': '200'}
 
-        next_href = favoriters['next_href']
-        favoriters = requests.get(next_href).json()
+for i in range(0, 1):
+    if 'next_href' not in favoriters.keys() or 'collection' not in favoriters.keys():
+        break
 
-        favoritersIds = ([favoriter['id'] for favoriter in favoriters['collection']])
+    next_href = favoriters['next_href']
+    favoriters = requests.get(next_href).json()
 
-        if i%10 == 0:
-            print('Inner loop: ' + str(i))
+    for favoriter in favoriters['collection']:
+        user = {'id': favoriter['id']}
+        favoriteTracks = requests.get('https://api.soundcloud.com/users/' + str(favoriter['id']) + '/favorites?client_id=' + client_id, params).json()
 
+        favoriteTracksList = []
 
-    trackInfo = {}
-    trackInfo['id'] = track['id']
-    trackInfo['genre'] = track['genre']
-    trackInfo['playback_count'] = track['playback_count']
-    trackInfo['favoritings_count'] = track['favoritings_count']
-    trackInfo['favoriters'] = favoritersIds
-    result.append(trackInfo)
-    print('Outer loop: ' + str(count))
-    time.sleep(0.05)
+        for favoriteTrack in favoriteTracks:
+            favoriteTrackInfo = {}
+            favoriteTrackInfo['id'] = favoriteTrack['id']
+            favoriteTrackInfo['likes_count'] = favoriteTrack['likes_count']
+            favoriteTrackInfo['reposts_count'] = favoriteTrack['reposts_count']
+            favoriteTrackInfo['genre'] = favoriteTrack['genre']
+            favoriteTrackInfo['permalink_url'] = favoriteTrack['permalink_url']
+            favoriteTrackInfo['title'] = favoriteTrack['title']
+            favoriteTrackInfo['description'] = favoriteTrack['description']
+            favoriteTrackInfo['streamable'] = favoriteTrack['streamable']
+            favoriteTrackInfo['stream_url'] = favoriteTrack['stream_url']
+            favoriteTracksList.append(favoriteTrackInfo)
 
+        user['favoriteTracks'] = favoriteTracksList
+
+        result.append(user)
+        break
+    break
 
 with open(currentGenre + '.json', 'w', encoding='utf8') as json_file:
     json.dump(result, json_file, ensure_ascii=False)
