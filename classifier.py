8084f4c6dab78719ab09a7985557a3b51511f407
FredrikErikJohansson <fredrik96.johansson@gmail.com>
2019-12-18 10:31:39 +0100

    Added first version of score

--- classifier.py
+++ classifier.py
@@ -8,9 +8,26 @@ X_test = [[0.0, 0.0, 1.0]]
 
 knn = NearestNeighbors(n_neighbors=2)
 knn.fit(X)
-pred = knn.kneighbors(X_test,return_distance=False)
+pred = knn.kneighbors(X_test)
 
-for track in Y["favoriteTracks"][pred[0][0]]:
-    if track['genre'] == 'Pop' or track['genre'] == 'Rock' or track['genre'] == 'Hip-hop & Rap':
-        print(track['title']  + "     " + str(track['genre']))
+#TODO: add dictionary with track_ids and their number of occurrences
+#Weight algorithm for recommending tracks
+rec_tracks = {}
 
+for i in range(0,2):
+    dist = pred[0][0][i]
+    for track in Y["favorite_tracks"][pred[1][0][i]]:
+        #TODO: calculate score here
+        if track['track_id'] in rec_tracks:
+            rec_tracks[track['track_id']] -= track['likes_count'] / 2000000.0
+        else:
+            rec_tracks[track['track_id']] = track['likes_count'] / 1000000.0
+
+maxScore = {'track_id': 0, 'score': 0}
+
+for track in rec_tracks:
+    if rec_tracks[track] > maxScore['score']:
+        maxScore['score'] = rec_tracks[track]
+        maxScore['track_id'] = track
+
+print(maxScore)
