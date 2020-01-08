11d21c31c96c9a7bea34ee7f534c6320b9530d0c
johanforslund <johanforslund95@gmail.com>
2020-01-08 15:21:37 +0100

    Now possible to classify with all data

--- classifier.py
+++ classifier.py
@@ -1,12 +1,48 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import pandas as pd
+import re
 
-X = pd.read_json('./data/rockSplit.json')
-Y = pd.read_json('./data/rock.json')
+def read_data():
+    A = pd.read_json('./data/acoustic.json')
+    B = pd.read_json('./data/bass.json')
+    C = pd.read_json('./data/blues.json')
+    D = pd.read_json('./data/classical.json')
+    E = pd.read_json('./data/country.json')
+    F = pd.read_json('./data/dubstep.json')
+    G = pd.read_json('./data/edm.json')
+    H = pd.read_json('./data/folk.json')
+    I = pd.read_json('./data/hiphop.json')
+    J = pd.read_json('./data/house.json')
+    K = pd.read_json('./data/jazz.json')
+    L = pd.read_json('./data/latin.json')
+    M = pd.read_json('./data/metal.json')
+    N = pd.read_json('./data/pop.json')
+    O = pd.read_json('./data/rap.json')
+    P = pd.read_json('./data/rnb.json')
+    Q = pd.read_json('./data/rock.json')
+    R = pd.read_json('./data/singersongwriter.json')
+    S = pd.read_json('./data/soul.json')
+    T = pd.read_json('./data/soundtrack.json')
+    U = pd.read_json('./data/techno.json')
+    V = pd.read_json('./data/trap.json')
+    W = pd.read_json('./data/world.json')
+    return pd.concat([A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W]).drop_duplicates(subset='user_id').reset_index(drop=True)
+
+def clean_genre(s):
+    #TODO: also remove "and" and switch "raphiphop" to "hiphoprap"
+    regex = re.compile('[^a-zA-Z]')
+    s = regex.sub('', s)
+    return s.lower()
+
+
+X = pd.read_json('./data/splitAll2.json')
+print("Finished reading split")
+Y = read_data()
+print("Finished reading data")
 
 X_test = [[0] * 126]
-X_test[0][20] = 1.0
+X_test[0][99] = 1.0
 
 #X_test = np.random.dirichlet(np.ones(126), size=1)
 
@@ -15,13 +51,15 @@ X_test[0][20] = 1.0
 #X_test = [i/s for i in X_test]
 #X_test = [X_test]
 
-k = 4
+k = 16
 knn = NearestNeighbors(n_neighbors=k)
 
 knn.fit(X)
 pred = knn.kneighbors(X_test)
 #print(X_test)
-#print(X.iloc[pred[1][0][0]])
+print(X.iloc[pred[1][0][0]])
+print("Pred")
+print(pred)
 #TODO: add dictionary with track_ids and their number of occurrences
 #Weight algorithm for recommending tracks
 rec_tracks = {}
@@ -30,6 +68,11 @@ for i in range(0,k):
     #dist = pred[0][0][i]
     for track in Y["favorite_tracks"][pred[1][0][i]]:
         #TODO: calculate score here
+        #TODO: filter podcast and live
+        if track['genre'] and clean_genre(track['genre']) != 'rock': 
+            continue
+        if track['likes_count'] > 5000:
+            continue
         if track['id'] in rec_tracks:
             rec_tracks[track['id']] += 1
         else:
--- splitData.py
+++ splitData.py
@@ -2,13 +2,40 @@ import pandas as pd
 import json
 import re
 
+def read_data():
+    A = pd.read_json('./data/acoustic.json')
+    B = pd.read_json('./data/bass.json')
+    C = pd.read_json('./data/blues.json')
+    D = pd.read_json('./data/classical.json')
+    E = pd.read_json('./data/country.json')
+    F = pd.read_json('./data/dubstep.json')
+    G = pd.read_json('./data/edm.json')
+    H = pd.read_json('./data/folk.json')
+    I = pd.read_json('./data/hiphop.json')
+    J = pd.read_json('./data/house.json')
+    K = pd.read_json('./data/jazz.json')
+    L = pd.read_json('./data/latin.json')
+    M = pd.read_json('./data/metal.json')
+    N = pd.read_json('./data/pop.json')
+    O = pd.read_json('./data/rap.json')
+    P = pd.read_json('./data/rnb.json')
+    Q = pd.read_json('./data/rock.json')
+    R = pd.read_json('./data/singersongwriter.json')
+    S = pd.read_json('./data/soul.json')
+    T = pd.read_json('./data/soundtrack.json')
+    U = pd.read_json('./data/techno.json')
+    V = pd.read_json('./data/trap.json')
+    W = pd.read_json('./data/world.json')
+    return pd.concat([A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W]).drop_duplicates(subset='user_id').reset_index(drop=True)
+
 def clean_genre(s):
     #TODO: also remove "and" and switch "raphiphop" to "hiphoprap"
     regex = re.compile('[^a-zA-Z]')
     s = regex.sub('', s)
     return s.lower()
 
-X = pd.read_json('./data/jazz.json')
+X = read_data()
+
 genres = pd.read_json('./data/genres.json')['genres']
 
 genres = genres.values.tolist()
@@ -29,13 +56,15 @@ for user_id, favorite_tracks in X.itertuples(index=False):
         meanGenre[idx] += 1
 
     normalizer = sum(meanGenre)
+    if normalizer == 0:
+        normalizer = 1
     for i in range(0,len(meanGenre)):
         meanGenre[i] = meanGenre[i] / normalizer
 
-    result = {user_id: meanGenre}
+    #result = {user_id: meanGenre} not used anymore?
     
-    midGenre.append(result)
+    midGenre.append(meanGenre)
 
-with open('./data/jazzSplit.json', 'w', encoding='utf8') as json_file:
+with open('./data/splitAll2.json', 'w', encoding='utf8') as json_file:
     json.dump(midGenre, json_file, ensure_ascii=False)
 
